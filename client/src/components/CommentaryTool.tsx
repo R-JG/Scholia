585ac6f0371da7247325e4b49b03b0d35772890f
Robin Guerrier <robin.james.guerrier@gmail.com>
2023-04-30 21:55:50 -0400

    Make Page rendering bidirectional from initialPage

--- client/src/components/CommentaryTool.tsx
+++ client/src/components/CommentaryTool.tsx
@@ -15,9 +15,13 @@ const CommentaryTool = ({ user, selectedDocument }: Props) => {
     if (!user || !selectedDocument) return <div className='CommentaryTool'></div>;
 
     const [documentBlob, setDocumentBlob] = useState<Blob | null>(null);
-    const [totalPages, setTotalPages] = useState<number | null>(null);
+    const [documentIsLoaded, setDocumentIsLoaded] = useState<boolean>(false);
+    const [pageIsLoaded, setPageIsLoaded] = useState<boolean>(false);
+    const [totalPages, setTotalPages] = useState<number>(0);
     const [pageHeight, setPageHeight] = useState<number | null>(null);
-    const [pagesToRender, setPagesToRender] = useState<number>(0);
+    const [initialPageNum] = useState<number>(20);
+    const [previousPagesToRender, setPreviousPagesToRender] = useState<number>(0);
+    const [nextPagesToRender, setNextPagesToRender] = useState<number>(0);
 
     const documentContainerRef = useRef<HTMLDivElement>(null);
 
@@ -27,20 +31,69 @@ const CommentaryTool = ({ user, selectedDocument }: Props) => {
     }, []);
 
     useEffect(() => {
-        (totalPages && (totalPages < 3)) 
-            ? setPagesToRender(totalPages) 
-            : setPagesToRender(3);
-    }, [totalPages]);
+        if (!documentIsLoaded || !pageIsLoaded) return;
+        expandNextPages();
+        expandPreviousPages();
+    }, [pageIsLoaded]);
+
+    const calculatePagesToAdd = (direction: 'before-initial' | 'after-initial'): number => {
+        const unrenderedPages: number = (direction === 'before-initial') 
+            ? (initialPageNum - previousPagesToRender) 
+            : (totalPages - (initialPageNum + nextPagesToRender));
+        const amountToAdd: number = (unrenderedPages < 3) ? unrenderedPages : 3;
+        return amountToAdd;
+    };
+
+    const expandPreviousPages = (): void => {
+        setPreviousPagesToRender(previousPagesToRender + calculatePagesToAdd('before-initial'));
+    };
+
+    const expandNextPages = (): void => {
+        setNextPagesToRender(nextPagesToRender + calculatePagesToAdd('after-initial'));
+    };
 
     const handleScroll = (e: UIEvent<HTMLDivElement>): void => {
-        if (!totalPages || !pageHeight || !documentContainerRef.current) return;
-        if (totalPages === pagesToRender) return;
-        if (e.currentTarget.scrollTop >= (documentContainerRef.current.scrollHeight - pageHeight)) {
-            const pagesUnrendered: number = (totalPages - pagesToRender);
-            if (pagesUnrendered < 3) setPagesToRender(pagesToRender + pagesUnrendered);
-            if (pagesUnrendered >= 3) setPagesToRender(pagesToRender + 3);
+        if (!documentIsLoaded || !pageHeight || !documentContainerRef.current) return;
+        if ((previousPagesToRender + nextPagesToRender + 1) === totalPages) return;
+        if (((initialPageNum + nextPagesToRender) !== totalPages) 
+        && (e.currentTarget.scrollTop >= (documentContainerRef.current.scrollHeight - pageHeight))) {
+            expandNextPages();
         };
+        if (((initialPageNum - previousPagesToRender) !== 1) 
+        && (e.currentTarget.scrollTop <= pageHeight)) {
+            expandPreviousPages();
+        };
+
+        // prevent loading too many pages at once when agressively scrolling
     };
+
+    const createPages = (direction: 'before-initial' | 'after-initial'): JSX.Element[] => {
+        const pagesToRender: number = (direction === 'before-initial') 
+            ? previousPagesToRender : nextPagesToRender;
+        const getPageNum = (index: number): number => {
+            return (direction === 'before-initial') 
+                ? ((initialPageNum - previousPagesToRender) + (index + 1)) 
+                : (initialPageNum + (index + 1));
+        };
+        return (
+            Array.from({ length: pagesToRender }).map((_el, index) => {
+                const pageNum = getPageNum(index);
+                return (
+                    <Page 
+                        key={`${selectedDocument.documentName} page ${pageNum}`}
+                        className='document--pdf-page'
+                        pageNumber={pageNum} 
+                        renderAnnotationLayer={false}
+                        width={documentContainerRef.current?.clientWidth}
+                    />
+                );
+            })
+        );
+    };
+
+    console.log('TOTAL PAGES:   ', totalPages);
+    console.log('PREVIOUS:   ', previousPagesToRender);
+    console.log('NEXT:   ', nextPagesToRender);
 
     return (
         <div className='CommentaryTool'>
@@ -53,24 +106,24 @@ const CommentaryTool = ({ user, selectedDocument }: Props) => {
                 <Document 
                     className='document--pdf' 
                     file={documentBlob}
-                    onLoadSuccess={(pdf) => setTotalPages(pdf.numPages)}
+                    onLoadSuccess={(pdf) => {
+                        setTotalPages(pdf.numPages);
+                        setDocumentIsLoaded(true);
+                    }}
                 >
+                    {createPages('before-initial')}
                     <Page 
+                        key={`${selectedDocument.documentName} page ${initialPageNum}`}
                         className='document--pdf-page'
-                        pageNumber={1} 
+                        pageNumber={initialPageNum} 
                         renderAnnotationLayer={false}
                         width={documentContainerRef.current?.clientWidth}
-                        onLoadSuccess={(page) => setPageHeight(page.height)}
+                        onLoadSuccess={(page) => {
+                            setPageHeight(page.height);
+                            setPageIsLoaded(true);
+                        }}
                     />
-                    {Array.from({ length: (pagesToRender - 1) }).map((_el, index) => 
-                        <Page 
-                            key={`${selectedDocument.documentName} page ${index + 2}`}
-                            className='document--pdf-page'
-                            pageNumber={index + 2} 
-                            renderAnnotationLayer={false}
-                            width={documentContainerRef.current?.clientWidth}
-                        />
-                    )}
+                    {createPages('after-initial')}
                 </Document>}
             </div>
         </div>
