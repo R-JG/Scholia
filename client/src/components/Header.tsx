ad19ae994803dab6a08f6c1fa376df7fd60e328f
Robin Guerrier <robin.james.guerrier@gmail.com>
2023-05-27 02:37:23 -0400

    Add app title to header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -11,18 +11,20 @@ const Header = ({
     logout 
     }: Props) => {
 
-    if (!user) return <div className='Header inactive'></div>;
-
     return (
         <header className='Header'>
-            <h4 className='Header--username'>
-                {user.username}
-            </h4>
-            <button 
-                className='Header--logout-button' 
-                onClick={logout}>
-                Logout
-            </button>
+            <h1 className='Header--title'>SCHOLIA</h1>
+            {user &&
+            <div className='Header--user-section'>
+                <h4 className='Header--username'>
+                    {user.username}
+                </h4>
+                <button 
+                    className='Header--logout-button' 
+                    onClick={logout}>
+                    Logout
+                </button>
+            </div>}
         </header>
     );
 };
