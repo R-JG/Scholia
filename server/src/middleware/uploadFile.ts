1e9f593458c65a0d28902e6c5f3d1fec8389be07
Robin Guerrier <robin.james.guerrier@gmail.com>
2023-05-28 02:00:07 -0400

    Add uuid to multer filename creation

--- server/src/middleware/uploadFile.ts
+++ server/src/middleware/uploadFile.ts
@@ -1,9 +1,10 @@
 import multer from 'multer';
+import { v4 as uuidv4 } from 'uuid';
 import { DOCUMENT_DIR_FILE_PATH } from '../serverUtils/config';
 
 const storage = multer.diskStorage({
     destination: (_request, _file, cb) => cb(null, DOCUMENT_DIR_FILE_PATH),
-    filename: (_request, file, cb) => cb(null, file.originalname)
+    filename: (_request, file, cb) => cb(null, `${uuidv4()}_${file.originalname}`)
 });
 
 const upload = multer({ storage });
